5d8f4952414fe02fb55aecc278ed8072a35bdfdd
To'lqinbek Rasuljonov <rasuljonovtolqinbek3@gmail.com>
2026-02-16 01:59:51 +0500

    A005 masala c++

---
+++ A0005.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	long long A , B , C , N;
+	cin >> N ;
+	cin >> A >> B >> C ;
+	if( N <= A+B+C )
+	   cout << "Yes";
+	else 
+	   cout << "No";
+return 0 ; 
+}
